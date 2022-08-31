a665dfbb0d4ee734a7dddeeb3f28eb521201aec7
Kevin Vicencio <kevinarnoldcode@gmail.com>
2022-08-31 14:16:37 -0700

    wrapped app in apollo-client and initialized it, also added avatar and postbox components

---
+++ components/Avatar.tsx
@@ -0,0 +1,22 @@
+import { useSession } from 'next-auth/react';
+import React from 'react';
+import Image from 'next/image';
+
+type Props = {
+    seed?: string
+    large?: boolean
+}
+
+function Avatar({ seed, large }: Props) {
+    const { data: session } = useSession()
+  return (
+    <div className={`relative overflow-hidden h-10 w-10 rounded-full border-gray-300 bg-white ${ large && 'h-20 w-20'}`}>
+        <Image
+            layout='fill'
+            src={ seed|| `https://avatars.dicebear.com/api/bottts/${session?.user?.name || 'placeholder'}.svg`}
+        />
+    </div>
+  )
+}
+
+export default Avatar
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import PostBox from '../components/PostBox';
 
 const Home: NextPage = () => {
   return (
@@ -8,6 +9,13 @@ const Home: NextPage = () => {
         <title>Seenit</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      {/* postbox */}
+      <PostBox />
+
+      <div>
+        {/* feed */}
+      </div>
     </div>
   )
 }
