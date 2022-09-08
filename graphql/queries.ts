e3cb42e2ee40fd8e65b2e312d57c04c81055481d
Kevin Vicencio <kevinarnoldcode@gmail.com>
2022-09-07 17:06:00 -0700

    finished UI and have simple back end ready for v1 deployment

--- graphql/queries.ts
+++ graphql/queries.ts
@@ -12,6 +12,16 @@ export const GET_ALL_VOTES_BY_POST_ID = gql`
     }
 `
 
+export const GET_SUBSEENITS_WITH_LIMIT = gql`
+    query MyQuery($limit: Int!) {
+        getSubseenitListLimit(limit: $limit) {
+            created_at
+            id
+            topic
+        }
+    }
+`
+
 export const GET_POST_BY_POST_ID = gql`
     query myQuery($post_id: ID!) {
         getPostListByPostId(post_id: $post_id) {
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,22 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import PostBox from '../components/PostBox';
 import Feed from '../components/Feed';
+import { useQuery } from '@apollo/client';
+import { GET_SUBSEENITS_WITH_LIMIT } from '../graphql/queries';
+import SubseenitRow from '../components/SubseenitRow';
 
 const Home: NextPage = () => {
+  const {data, error} = useQuery(GET_SUBSEENITS_WITH_LIMIT, {
+    variables: {
+      limit: 10,
+    }
+  })
+
+  const subseenits: Subseenit[] = data?.getSubseenitListLimit;
+
+  console.log(subseenits)
+  console.log(error)
+
   return (
     <div className="my-7 mx-auto max-w-5xl">
       <Head>
@@ -11,18 +25,19 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      {/* postbox */}
       <PostBox />
 
       <div className='flex'>
-        {/* feed */}
         <Feed />
 
         <div className='sticky top-36 mx-5 mt-5 hidden h-fit min-w-[300px] rounded-md border border-gray-300 bg-white lg:inline'>
-          <p>Top Communities</p>
+          <p className='text-md mb-1 p-4 pb-3 font-bold'>Top Communities</p>
 
           <div>
           {/* List subseenits */}
+            {subseenits?.map((subseenit, i) => (
+              <SubseenitRow key={subseenit.id} topic={subseenit.topic} index={i} />
+            ))}
           </div>
         </div>
       </div>
